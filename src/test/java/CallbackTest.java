2d10868a9e11776d105f22f3961b803391ec1c8b
Radmir <radmirka11@gmail.com>
2022-10-04 00:27:49 +0300

    deleting drivers and replace success text selector

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -45,7 +45,7 @@ public class CallbackTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79997771100");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success'")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
